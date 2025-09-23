4bd2f56c1576421f5403cb5fdb955abe15bb3cc1
ilaydaufelek <ilaydaufelek@gmail.com>
2025-09-23 15:50:04 +0300

    initial commit

---
+++ tests/common.test.tsx
@@ -0,0 +1,10 @@
+
+import {render} from "@testing-library/react"
+import "jest-canvas-mock";
+import  Button  from "../src/components/button";
+
+describe('Common render',()=>{
+    it("renders without crashing", ()=>{
+        render(<Button/>)
+    })
+})
