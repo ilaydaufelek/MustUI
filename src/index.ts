a57b2470f7f56bf9a7b226c541b3e841398c30bb
ilaydaufelek <ilaydaufelek@gmail.com>
2025-09-23 16:42:13 +0300

    Enable tree-shaking and update package version to 1.0.1

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,1 @@
-import  Button  from "./components/button";
-export default Button
+export * from './components';
--- tests/common.test.tsx
+++ tests/common.test.tsx
@@ -1,7 +1,7 @@
 
 import {render} from "@testing-library/react"
 import "jest-canvas-mock";
-import  Button  from "../src/components/button";
+import  Button  from "../src/components/Button/index";
 
 describe('Common render',()=>{
     it("renders without crashing", ()=>{
