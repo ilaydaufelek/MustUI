61c325724c8311a650f5eb90d5e53fb0269e8401
ilaydaufelek <ilaydaufelek@gmail.com>
2025-10-02 16:15:09 +0300

    Button component updated

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,52 +1,53 @@
-import React from "react";
 import clsx from "clsx";
 
-type ButtonProps = {
-  children?: React.ReactNode;
-  onClick?: () => void;
-  variant?: "primary" | "secondary" | "danger";
-  size?: "sm" | "md" | "lg";
-  disabled?: boolean;
-  className?: string;
-};
+type  ButtonProps={
+    children?:React.ReactNode;
+    onClick?: () => void;
+    size?:'sm'| 'md' | 'lg'   ;
+    variant?:"primary" | "outline" | "secondary" | "danger";
+    disabled?:boolean;
+    className?:string;
+}
 
- const Button: React.FC<ButtonProps> = ({
-  children,
-  onClick,
-  variant = "primary",
-  size = "md",
-  disabled = false,
-  className,
-}) => {
-  const baseStyles =
-    "rounded-md font-medium focus:outline-none focus:ring-2 focus:ring-offset-2 transition-colors";
+ const Button=({
+children,
+onClick, 
+size="md",
+variant="primary",
+disabled=false,
+className
+}:ButtonProps)=>{
+     
+    const baseStyles="rounded-md font-medium  transition-colors"
 
-  const variants: Record<string, string> = {
-    primary: "bg-blue-600 text-white hover:bg-blue-700 focus:ring-blue-500",
-    secondary: "bg-gray-200 text-gray-800 hover:bg-gray-300 focus:ring-gray-400",
-    danger: "bg-red-600 text-white hover:bg-red-700 focus:ring-red-500",
-  };
+    const sizes: Record<string,string>={
+      "sm":"px-2 py-1 text-sm" ,
+      "md":"px-4 py-2 text-base",
+      "lg":"px-6 py-4 text-lg",
+      
+    }
 
-  const sizes: Record<string, string> = {
-    sm: "px-2 py-1 text-sm",
-    md: "px-4 py-2 text-base",
-    lg: "px-6 py-3 text-lg",
-  };
-
-  return (
-    <button
-      onClick={onClick}
-      disabled={disabled}
-      className={clsx(
+    const variants:Record<string,string>={
+    primary: "dark:bg-white bg-zinc-900  dark:text-black text-white dark:hover:bg-white/90 hover:bg-zinc-900/90 cursor-pointer ",
+    secondary: "bg-gray-200 text-gray-900 hover:bg-gray-300 cursor-pointer ",
+    outline:"border border-zinc-300 cursor-pointer",
+    danger: "bg-red-800 text-white hover:bg-red-900 cursor-pointer",
+    }
+    return(
+        <button 
+        onClick={onClick} 
+        disabled={disabled}
+        className={clsx(
         baseStyles,
-        variants[variant],
         sizes[size],
+        variants[variant],
         disabled && "opacity-50 cursor-not-allowed",
         className
-      )}
-    >
-      {children}
-    </button>
-  );
-};
-export default Button
+        )}>
+         {children}
+
+        </button>
+    )
+}
+
+export default Button;
