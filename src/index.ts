997c6e1343ad1c59c2e9773801d8e951426930e8
ilaydaufelek <ilaydaufelek@gmail.com>
2025-12-18 16:54:05 +0300

    feat!: switch to subpath exports for tree-shaking

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,1 @@
-export * from "./components/Button";
-export * from "./components/Input"
-export * from "./components/Dialog"
-export * from "./components/Accordion"
-export * from "./components/Sheet"
+export {}
